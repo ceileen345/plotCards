7052f8b500be9696e6620c925d0a6302065cb38c
ceileen345 <ceileen345@gmail.com>
2016-10-26 17:43:15 -0500

    Fixes javascript events not firing from dynamically created buttons and removes save button

--- plotcards.js
+++ plotcards.js
@@ -1,14 +1,24 @@
-$( document ).ready(function() {
-    $(".addafter").click(function(){
-        var addedCard = '<div class = "col-sm-3 col-xs-6"><div class = "added-card"><textarea class = "cardtitlebox">Title</textarea><button type="button" class = "btn btn-xs deletebutton">Delete</button><textarea class = "cardtext"></textarea><button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-ok"></span> Save</button><button type="button" class="btn btn-default btn-sm addafter2" onclick="addCard()">Add Event <span class="glyphicon glyphicon-arrow-right"></span> </button></div></div>';
-        $(this).parent().parent().after($(addedCard).fadeIn(1000));
-        $(".deletebutton").click(function(){
-            $(this).parent().parent().fadeOut(1000);
-        });        
-        $(".addafter2").click(function(){ $(this).parent().parent().after($(addedCard).fadeIn(1000));
-          $(".deletebutton").click(function(){
-            $(this).parent().parent().fadeOut(1000);
-        });
-                                        });
-        });
+
+$(document).on('click', '.deletebutton', function(){
+     $(this).parent().parent().fadeOut(500, function() { $(this).remove(); });
+  });
+  
+$(document).on('click', '.addafter', function(){
+    var addedCard = '<div class = "col-sm-3 col-xs-6"><div class = "added-card"><textarea class = "cardtitlebox savebox">Title</textarea><textarea class = "cardtext autoExpand"></textarea><button type="button" class = "btn btn-xs deletebutton">Delete</button><button type="button" class="btn btn-default btn-sm addafter" onclick="addCard()">Add Event <span class="glyphicon glyphicon-arrow-right"></span></button></div></div>';
+     $(this).parent().parent().after($(addedCard).fadeIn(1000));
+  });
+
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows;
     });
+
